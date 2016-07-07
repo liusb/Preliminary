769c90e1190efb57839328b10ebe16960e6124b7
liusb <liushaobo5@gmail.com>
2016-07-07 12:19:06 +0800

    use ThreadLocal kryo object

--- src/main/java/com/alibaba/middleware/race/RaceUtils.java
+++ src/main/java/com/alibaba/middleware/race/RaceUtils.java
@@ -12,6 +12,13 @@ public class RaceUtils {
      * @param object
      * @return
      */
+
+    private static final ThreadLocal<Kryo> kryos = new ThreadLocal<Kryo>() {
+        protected Kryo initialValue() {
+            return new Kryo();
+        }
+    };
+
     public static byte[] writeKryoObject(Object object) {
         Output output = new Output(1024);
         Kryo kryo = new Kryo();
@@ -24,15 +31,15 @@ public class RaceUtils {
     }
 
     public static <T> T readKryoObject(Class<T> tClass, byte[] bytes) {
-        Kryo kryo = new Kryo();
+        Kryo kryo = kryos.get();
         Input input = new Input(bytes);
-        input.close();
         T ret = kryo.readObject(input, tClass);
+        input.close();
         return ret;
     }
 
     public static long millisToSecondsOfMinute(long milliseconds) {
-        return milliseconds/1000/60*60;
+        return (milliseconds/60000)*60;
     }
 
 }
